8f609594d772687f10482ec75825a76a4a021efe
Fábio Kenji Matsuda <fabio@fkmatsuda.dev>
2022-08-08 23:58:00 -0300

    ci: improves test coverage

--- sqlexecutor/src/test/java/dev/fkmatsuda/spring/jdbc/SqlExecutorProviderTest.java
+++ sqlexecutor/src/test/java/dev/fkmatsuda/spring/jdbc/SqlExecutorProviderTest.java
@@ -20,7 +20,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import dev.fkmatsuda.spring.jdbc.SqlExecutorProvider.SqlExecutor;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {SqlExecutorProviderTestConfig.class})
+@ContextConfiguration(classes = { SqlExecutorProviderTestConfig.class })
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 public class SqlExecutorProviderTest {
 
@@ -52,8 +52,8 @@ public class SqlExecutorProviderTest {
         assertNotNull(sqlExecutorProvider);
         sqlExecutorProvider.forSql("CREATE TABLE test2 (id INTEGER);").executeDDL();
         SqlExecutor insertQuery = sqlExecutorProvider.forSql("INSERT INTO test2 (id) VALUES (:id)");
-        RequiredValueException e = assertThrows(RequiredValueException.class, 
-        () -> insertQuery.update());
+        RequiredValueException e = assertThrows(RequiredValueException.class,
+                () -> insertQuery.update());
         assertEquals("Parameters is required for this operation", e.getMessage());
     }
 
@@ -61,8 +61,8 @@ public class SqlExecutorProviderTest {
     void testCountInvalidQuery() throws SqlException, InvalidArgumentException {
         assertNotNull(sqlExecutorProvider);
         SqlExecutor executor = sqlExecutorProvider.forSql("CREATE TABLE test3 (id INTEGER);");
-        InvalidArgumentException e = assertThrows(InvalidArgumentException.class, 
-        () -> executor.count());
+        InvalidArgumentException e = assertThrows(InvalidArgumentException.class,
+                () -> executor.count());
         assertEquals("Cannot count current query result", e.getMessage());
     }
 
@@ -70,11 +70,11 @@ public class SqlExecutorProviderTest {
     void testSqlException() throws SqlException, InvalidArgumentException {
         assertNotNull(sqlExecutorProvider);
         SqlExecutor executor = sqlExecutorProvider.forSql("select from voyts");
-        SqlException e = assertThrows(SqlException.class, 
-        () -> executor.update());
+        SqlException e = assertThrows(SqlException.class,
+                () -> executor.update());
         assertNotNull(e);
     }
-    
+
     @Test
     void testValidQueryWithParams() throws SqlException, InvalidArgumentException {
         assertNotNull(sqlExecutorProvider);
@@ -98,8 +98,8 @@ public class SqlExecutorProviderTest {
     void testDDLException() {
         assertNotNull(sqlExecutorProvider);
         SqlExecutor executor = sqlExecutorProvider.forSql("CREATE voyts;;");
-        SqlException e = assertThrows(SqlException.class, 
-        () -> executor.executeDDL());
+        SqlException e = assertThrows(SqlException.class,
+                () -> executor.executeDDL());
         assertNotNull(e);
     }
 
@@ -144,36 +144,36 @@ public class SqlExecutorProviderTest {
     @Test()
     void testQueryNullValues() throws SqlException, InvalidArgumentException {
         assertNotNull(sqlExecutorProvider);
-        sqlExecutorProvider.forSql("CREATE TABLE test6 (" + 
-            "id INTEGER, str text, " + 
-            "b_value boolean, " + 
-            "i_value int4, " + 
-            "l_value int8, " +
-            "n_value numeric(15,5));").executeDDL();
+        sqlExecutorProvider.forSql("CREATE TABLE test6 (" +
+                "id INTEGER, str text, " +
+                "b_value boolean, " +
+                "i_value int4, " +
+                "l_value int8, " +
+                "n_value numeric(15,5));").executeDDL();
         SqlExecutor insertQuery = sqlExecutorProvider.forSql(
-            "INSERT INTO test6 (id) VALUES (:id)");
+                "INSERT INTO test6 (id) VALUES (:id)");
         for (int i = 0; i < 16; i++) {
             insertQuery.setParameter("id", i).update();
         }
 
         Long lValue = sqlExecutorProvider.forSql("select l_value from test6 where id = :id")
-            .setParameter("id", 1L).queryForLong();
+                .setParameter("id", 1L).queryForLong();
         assertNull(lValue);
 
         BigDecimal nValue = sqlExecutorProvider.forSql("select n_value from test6 where id = :id")
-            .setParameter("id", 1L).queryForBigDecimal();
+                .setParameter("id", 1L).queryForBigDecimal();
         assertNull(nValue);
 
         String strValue = sqlExecutorProvider.forSql("select str from test6 where id = :id")
-            .setParameter("id", 1L).queryForString();
+                .setParameter("id", 1L).queryForString();
         assertNull(strValue);
 
         Boolean bValue = sqlExecutorProvider.forSql("select b_value from test6 where id = :id")
-            .setParameter("id", 1L).queryForBoolean();
+                .setParameter("id", 1L).queryForBoolean();
         assertFalse(bValue);
 
         Integer iValue = sqlExecutorProvider.forSql("select i_value from test6 where id = :id")
-            .setParameter("id", 1L).queryForInt();
+                .setParameter("id", 1L).queryForInt();
         assertNull(iValue);
 
         List<Long> lValues = sqlExecutorProvider.forSql("select l_value from test6").queryForLongList();
@@ -181,7 +181,40 @@ public class SqlExecutorProviderTest {
             assertNull(l);
         }
 
+    }
 
+    @Test()
+    void testEmptyQueryResult() throws SqlException, InvalidArgumentException {
+        assertNotNull(sqlExecutorProvider);
+        sqlExecutorProvider.forSql("CREATE TABLE test7 (" +
+                "id INTEGER, str text, " +
+                "b_value boolean, " +
+                "i_value int4, " +
+                "l_value int8, " +
+                "n_value numeric(15,5));").executeDDL();
+
+        Long lValue = sqlExecutorProvider.forSql("select l_value from test7 where id = :id")
+                .setParameter("id", 1L).queryForLong();
+        assertNull(lValue);
+
+        BigDecimal nValue = sqlExecutorProvider.forSql("select n_value from test7 where id = :id")
+                .setParameter("id", 1L).queryForBigDecimal();
+        assertNull(nValue);
+
+        String strValue = sqlExecutorProvider.forSql("select str from test7 where id = :id")
+                .setParameter("id", 1L).queryForString();
+        assertNull(strValue);
+
+        Boolean bValue = sqlExecutorProvider.forSql("select b_value from test7 where id = :id")
+                .setParameter("id", 1L).queryForBoolean();
+        assertFalse(bValue);
+
+        Integer iValue = sqlExecutorProvider.forSql("select i_value from test7 where id = :id")
+                .setParameter("id", 1L).queryForInt();
+        assertNull(iValue);
+
+        List<Long> lValues = sqlExecutorProvider.forSql("select l_value from test7").queryForLongList();
+        assertTrue(lValues.isEmpty());
     }
 
 }
