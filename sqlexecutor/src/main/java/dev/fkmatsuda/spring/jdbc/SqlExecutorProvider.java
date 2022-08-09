33d024c14f6d5ad7ccbf2396a15ae9b8e2af512d
Fábio Kenji Matsuda <fabio@fkmatsuda.dev>
2022-08-09 00:28:38 -0300

    fix: ORDER_LIMIT_PATTERN improvement

--- sqlexecutor/src/main/java/dev/fkmatsuda/spring/jdbc/SqlExecutorProvider.java
+++ sqlexecutor/src/main/java/dev/fkmatsuda/spring/jdbc/SqlExecutorProvider.java
@@ -53,7 +53,7 @@ public class SqlExecutorProvider {
         private static final Pattern QUERY_COUNT_PATTERN = Pattern.compile("(^\\s*select\\s+)(.*?)(\\s+from\\s+.*)($)",
                 Pattern.DOTALL | Pattern.MULTILINE | Pattern.CASE_INSENSITIVE);
         private static final Pattern ORDER_LIMIT_PATTERN = Pattern.compile(
-                "(^.*?)(\\s+order\\s+by\\s+|\\s+limit\\s+|\\s+offset\\s+)(.*)($)",
+                "(^.*?)((\\s+order\\s+by|\\s+limit\\s+)|(\\s+offset))(\\s+.*)($)",
                 Pattern.DOTALL | Pattern.MULTILINE | Pattern.CASE_INSENSITIVE);
 
         private final String sql;
